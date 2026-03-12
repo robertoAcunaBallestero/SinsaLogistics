2626c72bd760763b12e029403580784678e45e26
robertoAcunaBallestero <robertoacu953@gmail.com>
2026-03-12 11:04:45 -0600

    Carpetas y archivos

---
+++ SinsaLogistic/src/main/java/com/example/domain/Cliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.domain;
+
+/**
+ *
+ * @author Administrador(a)
+ */
+public class Cliente {
+    
+}
