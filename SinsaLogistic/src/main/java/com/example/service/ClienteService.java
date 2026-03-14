d411dc4c3980294039b611d5403424e6cbe76441
Angelss555 <vangelfelipe01@gmail.com>
2026-03-13 21:10:28 -0600

    Create ClienteRepository for Cliente entity
    Added ClienteRepository interface for database operations.

--- SinsaLogistic/src/main/java/com/example/service/ClienteService.java
+++ SinsaLogistic/src/main/java/com/example/service/ClienteService.java
@@ -4,10 +4,20 @@
  */
 package com.example.service;
 
+import com.sinsa.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 /**
  *
  * @author Administrador(a)
  */
-public class ClienteService {
+
+
+@Repository
+public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
     
+    //Usamos Spring para crea la consulta automáticamente por el nombre del método
+    //Se usa el Útil para cuando un cliente quiera iniciar sesión o buscar sus datos
+    Cliente findByCorreo(String correo);
 }
