9be4dd884dc353860310c3f77e3429054d6f8d19
Angelss555 <vangelfelipe01@gmail.com>
2026-03-13 20:23:57 -0600

    Add getters and setters for Material class

--- SinsaLogistic/src/main/java/com/example/domain/Material.java
+++ SinsaLogistic/src/main/java/com/example/domain/Material.java
@@ -41,12 +41,16 @@ public class Material {
     //Creamos los Getters & Setters
     public Integer getIdMaterial() { return idMaterial; }
     public void setIdMaterial(Integer idMaterial) { this.idMaterial = idMaterial; }
+    
     public String getNombre() { return nombre; }
     public void setNombre(String nombre) { this.nombre = nombre; }
+    
     public Integer getPrecio() { return precio; }
     public void setPrecio(Integer precio) { this.precio = precio; }
+    
     public Integer getStock() { return stock; }
     public void setStock(Integer stock) { this.stock = stock; }
+    
     public String getDescripcion() { return descripcion; }
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 }
