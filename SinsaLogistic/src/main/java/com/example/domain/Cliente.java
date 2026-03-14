632d9f429565178063cd0b193cd7bf6a408341e1
Angelss555 <vangelfelipe01@gmail.com>
2026-03-13 20:53:37 -0600

    Update Cliente.java

--- SinsaLogistic/src/main/java/com/example/domain/Cliente.java
+++ SinsaLogistic/src/main/java/com/example/domain/Cliente.java
@@ -4,10 +4,42 @@
  */
 package com.example.domain;
 
+import jakarta.persistence.*;
+
 /**
  *
  * @author Administrador(a)
  */
+
+@Entity
+@Table(name = "Cliente")
 public class Cliente {
+
+    //Creamos la tabla
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_cliente")
+    private Integer idCliente
+
+    //Creamos los atributos de la tabla de cliente
+    private String nombre;
+    private String correo;
+    private String direccion;
+
+    //Creamos el constructor
+    public Cliente() {
+    }
+
+    //Definimos los Getters & Setters
+    public Integer getIdCliente() { return idCliente; }
+    public void setIdCliente(Integer idCliente) { this.idCliente = idCliente; }
     
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    
+    public String getCorreo() { return correo; }
+    public void setCorreo(String correo) { this.correo = correo; }
+    
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
 }
