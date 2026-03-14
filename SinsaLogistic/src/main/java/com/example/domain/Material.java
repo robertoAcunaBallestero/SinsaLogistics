cb009adc7cb8d63dce0a15ef74038383911b5089
Angelss555 <vangelfelipe01@gmail.com>
2026-03-13 20:23:38 -0600

    Update Material.java

--- SinsaLogistic/src/main/java/com/example/domain/Material.java
+++ SinsaLogistic/src/main/java/com/example/domain/Material.java
@@ -4,10 +4,49 @@
  */
 package com.example.domain;
 
+import jakarta.persistence.*;
+
 /**
  *
  * @author Administrador(a)
  */
+
+@Entity
+@Table(name = "Material")
 public class Material {
-    
+
+    //Creamos la tabla
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_material")
+    private Integer idMaterial;
+
+    //Definimos los atributos de la tabla
+    private String nombre;
+    private Integer precio;
+    private Integer stock;
+    private String descripcion;
+
+    //Definimos la relación (Lo dejé así por si no se ha definido)
+    /*
+    @ManyToOne
+    @JoinColumn(name = "id_proveedor")
+    private Proveedor proveedor;
+    */
+
+    //Creamos el constructor 
+    public Material() {
+    }
+
+    //Creamos los Getters & Setters
+    public Integer getIdMaterial() { return idMaterial; }
+    public void setIdMaterial(Integer idMaterial) { this.idMaterial = idMaterial; }
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public Integer getPrecio() { return precio; }
+    public void setPrecio(Integer precio) { this.precio = precio; }
+    public Integer getStock() { return stock; }
+    public void setStock(Integer stock) { this.stock = stock; }
+    public String getDescripcion() { return descripcion; }
+    public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
 }
