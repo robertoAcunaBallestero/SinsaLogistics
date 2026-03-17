2c7752f4239b8a52a447a73ea1854cc1f962124d
pablorv-a11y <pr932019@gmail.com>
2026-03-17 08:43:24 -0600

    Update ClienteService.java

--- SinsaLogistic/src/main/java/com/example/service/ClienteService.java
+++ SinsaLogistic/src/main/java/com/example/service/ClienteService.java
@@ -1,18 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.example.service;
-
-import com.sinsa.domain.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 /**
  *
- * @author Administrador(a)
+ * 
  */
 
+package com.example.repository;
+
+import com.example.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
