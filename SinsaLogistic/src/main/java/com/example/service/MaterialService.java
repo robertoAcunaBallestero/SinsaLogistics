15de17a8f5a802536414992bbc360c8fba220450
pablorv-a11y <pr932019@gmail.com>
2026-03-17 08:52:26 -0600

    Update MaterialService.java

--- SinsaLogistic/src/main/java/com/example/service/MaterialService.java
+++ SinsaLogistic/src/main/java/com/example/service/MaterialService.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.example.service;
 
 /**
@@ -9,18 +6,18 @@ package com.example.service;
  * @author Administrador(a)
  */
 
-import com.sinsa.domain.Material;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
+import com.example.domain.Material;
 import java.util.List;
 
-@Repository
-public interface MaterialRepository extends JpaRepository<Material, Integer> {
+public interface MaterialService {
 
-    //Cumplimos la Historia de Usuario de "Alertas de Stock"
-    //En esta consulta te devuelve todos los materiales que tengan el stock igual o menor a un número.
-    @Query("SELECT m FROM Material m WHERE m.stock <= :limiteMinimo")
-    List<Material> buscarMaterialesConBajoStock(@Param("limiteMinimo") Integer limiteMinimo);
+    public List<Material> getMateriales();
+
+    public Material getMaterial(Material material);
+
+    public void save(Material material);
+
+    public void delete(Material material);
+
+    public List<Material> getMaterialesStockBajo(Integer stock);
 }
