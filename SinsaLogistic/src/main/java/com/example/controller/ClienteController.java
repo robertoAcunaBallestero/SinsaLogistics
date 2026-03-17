d04d1ee46ae8661cf88f40f53e01030574debb19
pablorv-a11y <pr932019@gmail.com>
2026-03-16 22:35:11 -0600

    Update ClienteController.java
    Se agregar los controladores de cliente v1

--- SinsaLogistic/src/main/java/com/example/controller/ClienteController.java
+++ SinsaLogistic/src/main/java/com/example/controller/ClienteController.java
@@ -1,13 +1,61 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.example.controller;
 
 /**
  *
- * @author Administrador(a)
+ * @Pablo Romero
  */
+
+package com.example.controller;
+
+import com.example.domain.Cliente;
+import com.example.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequesteMapping("/cliente")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    // 1. Se procede a enListar clientes
+    @GetMapping("/listado")
+    public String listado(Model model) {
+        var clientes = clienteService.getClientes();
+        model.addAttribute("clientes", clientes);
+        return "cliente/listado";
+
+    }
+
+    // 2. Se procede a mostrar formulario de nuevo cliente
+    @GetMapping("/nuevo")
+    public String nuevoCliente(Cliente cliente) {
+        return "cliente/modifica";
+
+    }
+
+    //3. Se procede a guardar un cliente nuevo o editado
+    @PostMapping("/guardar")
+    public String guadarCliente(Cliente cliente){
+        clienteService.save(cliente);
+        return "redirect:/cliente/listado";
+    }
+
+    //4. Se procede a editar un cliente 
+    @GetMapping("/modificar/{idCliente}")
+    public String modificarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCliente(cliente);
+        model.addAttribute("clliente", cliente);
+        return "cliente/modifica";
+    }
+
+    // 5. Se procede a eliminar cliente
+    @GetMapping("/eliminar/{idCliente}")
+    public String eliminarCliente(Cliente cliente){
+        clienteService.delete(cliente);
+        return"redirect:/cliente/listado";
+    }
     
 }
