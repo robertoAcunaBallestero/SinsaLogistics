00c5d5910e4ad6c713db9d57008a2b0919d24633
pablorv-a11y <pr932019@gmail.com>
2026-03-16 22:50:06 -0600

    Update MaterialController.java
    Se agregan los controladores principales de MaterialController V1.

--- SinsaLogistic/src/main/java/com/example/controller/MaterialController.java
+++ SinsaLogistic/src/main/java/com/example/controller/MaterialController.java
@@ -1,13 +1,66 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.example.controller;
-
 /**
  *
- * @author Administrador(a)
+ * @Pablo Romero
  */
+
+
+package com.example.controller;
+
+import com.example.domain.Material;
+import com.example.service.MaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/material")
+
 public class MaterialController {
+
+    @Autowired
+    private MaterialService materialService;
+
+    // 1. Se proceden a enlitar los materiales 
+
+    @GetMapping("/listado")
+    public String listado(Model model){
+        var materiales = materialService.getMateriales();
+        model.addAttribute("materiales", materiales);
+        return "material/listado";
+
+    }
+
+    // 2. Se procede a mostrar el formulario del nuevo material
+    @GetMapping("/nuevo")
+    public String nuevoMaterial(Material material) {
+        return "material/modifica";
+    }
+
+    // 3. Se procede a guadar el material 
+
+    @PostMapping("/guadar")
+    public String guadarMaterial(Material material) {
+        materialService.save(material);
+        return "redirect:/material/listado";
+
+    }
+
+    // 4. Se procede a editar el material
+
+    @GetMapping("/modificar/{idMaterial}")
+    public String modificarMaterial(Material material, Model model){
+        material = materialService.getMaterial(material);
+        model.addAttribute("material", material);
+        return "material/modifica";
+    }
+
+    //5. Se procede a eliminar el material 
+
+    @GetMapping("/eliminar/{idMaterial}")
+    public String eliminarMaterial(Material material){
+        materialService.delete(material);
+        return "redirect:/material/listado";
+    }
     
 }
