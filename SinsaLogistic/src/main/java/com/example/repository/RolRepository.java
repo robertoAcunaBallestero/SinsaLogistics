88aab5bcc78386bb8bfaca8e7c7ae765a3dcaa18
Angelss555 <vangelfelipe01@gmail.com>
2026-04-11 20:22:55 -0600

    Create RolRepository interface for Rol entity

---
+++ SinsaLogistic/src/main/java/com/example/repository/RolRepository.java
@@ -0,0 +1,11 @@
+package com.example.repository;
+
+import com.example.domain.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/*
+Angel Rodríguez
+*/
+
+public interface RolRepository extends JpaRepository<Rol, Integer> {
+}
